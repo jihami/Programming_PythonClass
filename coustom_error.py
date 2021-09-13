85042421d82446caead39b06ac5484ab355cf728
jzh04 <w2027@e-mirim.hs.kr>
2021-09-13 13:56:04 +0900

    InvalidLengthError

---
+++ coustom_error.py
@@ -0,0 +1,3 @@
+class InvalidLengthError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
--- moudle,package/baseball.py
+++ moudle,package/baseball.py
@@ -1,14 +1,20 @@
 from baseball_game_engine import make_answer, check
-
+from coustom_error import InvalidLengthError
 answer = make_answer()
 
 # 반복
 while True:
     # 질문
     guess = input('예상 숫자 : ')
-    # 숫자인지 확인
-    guess_int = int(guess)
-    print(guess_int)
+    # 숫자인지 아닌지 확인
+    try:
+        guess_int = int(guess)
+    except ValueError as e:
+        print('숫자를 입력하세요')
+        continue
+    if len(guess) != len(answer):
+        raise InvalidLengthError('정답의 길이와 다름니다. 3자리 수를 입력해주세요')
+
     # 결과 판정
     strike, ball = check(guess, answer)
     # 출력
