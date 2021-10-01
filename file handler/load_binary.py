eaf042d2b84f1521b185476478dfa10aa7c49a3c
jihami <w2027@e-mirim.hs.kr>
2021-10-01 16:10:54 +0900

    load_binary.py

--- file handler/load_binary.py
+++ file handler/load_binary.py
@@ -1,3 +1,6 @@
+with open("text.bin","wb") as f:
+    f.write(b"\xea\xb0\x80")
+input()
 with open("binary.bin", "rb") as f:
     data = f.read()
 print(data) #b"\xff\x00\x7f": 111111110000000001111111    \x -> 16진수하는 뜻
