feaa7a2a477fdc6e82c857844c57d55ca027ac4c
jihami <w2027@e-mirim.hs.kr>
2021-10-01 14:36:29 +0900

    파일처리 실습

---
+++ file handler/load_text.py
@@ -0,0 +1,4 @@
+f = open("text.txt","r",encoding="utf-8")
+data = f.read()
+f.close()
+print(data)
--- file handler/save_text.py
+++ file handler/save_text.py
@@ -1,6 +1,7 @@
-f = open("text.txt","w")
+f = open("text.txt","w", encoding="UTF-8")
 f.write("다빈")
 f.write("보라")
+f.write("\n")
 f.write("분홍")
 f.write("다빈")
 f.close()
