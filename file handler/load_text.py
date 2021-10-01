24ad330754c5db883d5280765e557cc781fc1864
jihami <w2027@e-mirim.hs.kr>
2021-10-01 14:46:56 +0900

    한줄씩 읽기

--- file handler/load_text.py
+++ file handler/load_text.py
@@ -5,6 +5,14 @@ f.close()
 print(data)
 print("한줄씩 읽기")
 f = open("text.txt", "r", encoding="utf-8")
-line = f.readline()
-f.close()
+line = f.readline() #line : 다빈보라\n 이 들어 잇음 -> textㅎ파일에 있는거임
 print(line)
+line = f.readline()
+print(line)
+
+while True:
+    line = f.read() #line : 다빈보라\n
+    if line == "" : #빈칸이라면 끝나라
+               break
+    print(line)
+f.close()
