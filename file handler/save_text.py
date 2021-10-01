778eff42cc1b042b1f51226acab4ba353c9b967b
jihami <w2027@e-mirim.hs.kr>
2021-10-01 15:41:01 +0900

    오타수정

--- file handler/save_text.py
+++ file handler/save_text.py
@@ -1,15 +1,15 @@
 f = open("text.txt","w", encoding="UTF-8")
-f.write("다빈")
-f.write("보라")
-f.write("\n")
+f.write("홍다빈")
 f.write("분홍")
-f.write("다빈")
+f.write("\n")
+f.write("유바롬")
+f.write("보라")
 f.close()
 
 with open("text.txt","w", encoding="UTF-8") as f:
-    f.write("다빈")
-    f.write("보라")
-    f.write("\n")
+    f.write("홍다빈")
     f.write("분홍")
-    f.write("다빈")
+    f.write("\n")
+    f.write("유바롬")
+    f.write("보라")
 #with를 벗어나면 자동으로 닫아줌
