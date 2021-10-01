fa25288bb8939a8217a2b8bf93eb4b91cea74b71
jihami <w2027@e-mirim.hs.kr>
2021-10-01 14:52:39 +0900

    한줄씩 읽고 줄별로 리스트

--- file handler/load_text.py
+++ file handler/load_text.py
@@ -5,14 +5,21 @@ f.close()
 print(data)
 print("한줄씩 읽기")
 f = open("text.txt", "r", encoding="utf-8")
-line = f.readline() #line : 다빈보라\n 이 들어 잇음 -> textㅎ파일에 있는거임
-print(line)
-line = f.readline()
-print(line)
+# line = f.readline() #line : 다빈보라\n 이 들어 잇음 -> textㅎ파일에 있는거임
+# print(line)
+# line = f.readline()
+# print(line)
 
 while True:
     line = f.read() #line : 다빈보라\n
     if line == "" : #빈칸이라면 끝나라
-               break
-    print(line)
+        break
+    print(line.rstrip()) #line.replace("\n","")
 f.close()
+
+print("전체를 한꺼번에 읽고, 줄 별로 리스트")
+f = open("text.txt", "r", encoding="utf-8")
+lines = f.readlines()
+f.close()
+for line in lines:
+    print(line.rstrip())
