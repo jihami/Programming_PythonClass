b77b3f3c578b4cf7e8d3f72cf49508399d3f91d4
jihami <w2027@e-mirim.hs.kr>
2021-10-01 16:06:26 +0900

    load_binary.py

--- file handler/load_binary.py
+++ file handler/load_binary.py
@@ -1,3 +1,3 @@
 with open("binary.bin", "rb") as f:
     data = f.read()
-print(data) #b"\xff\x00\x7f": 111111110000000001111111     \x -> 16진수하는 뜻
+print(data) #b"\xff\x00\x7f": 111111110000000001111111    \x -> 16진수하는 뜻
