5e1223b4dbf7e10df1c966ee37448bf35a953bc5
jihami <w2027@e-mirim.hs.kr>
2021-10-01 15:37:10 +0900

    save text, with

--- file handler/save_text.py
+++ file handler/save_text.py
@@ -5,3 +5,11 @@ f.write("\n")
 f.write("분홍")
 f.write("다빈")
 f.close()
+
+with open("text.txt","w", encoding="UTF-8") as f:
+    f.write("다빈")
+    f.write("보라")
+    f.write("\n")
+    f.write("분홍")
+    f.write("다빈")
+#with를 벗어나면 자동으로 닫아줌
